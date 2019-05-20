ed5032a952f15b41f8c7ad8e3022ccd37b61ec42
JaaySPatel <JaaySPatel@users.noreply.github.com>
2019-05-20 15:02:30 -0500

    Update MainActivity.java

--- ProChatApp/app/src/main/java/com/example/android/prochatapp/MainActivity.java
+++ ProChatApp/app/src/main/java/com/example/android/prochatapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.prochatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +13,27 @@ public class MainActivity extends AppCompatActivity {
     private static final String AUTH_SECRET = "UbLVUYqXAP3TsZn";
     private static final String ACCOUNT_KEY = "yZzhwz3LDsapvX7d_-L7";
 
+    Button btnLogin, btnSignUp;
+    EditText editUser, editPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //initialize the buttons and textinputs
+        btnLogin = (Button) findViewById(R.id.main_btbLogin);
+        btnSignUp = (Button) findViewById(R.id.main_btnSignUp);
+
+        editUser = (EditText) findViewById(R.id.main_editLogin);
+        editPassword = (EditText) findViewById(R.id.main_editPassword);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }//end onClick()
+        });
+
+    }//end onClick
+}//end class
