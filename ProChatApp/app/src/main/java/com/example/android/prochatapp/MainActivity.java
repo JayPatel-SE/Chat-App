8cd3e8c4ad5f2c6c4cdf1792ebc2d58aa7326e9c
JaaySPatel <JaaySPatel@users.noreply.github.com>
2019-05-20 14:54:45 -0500

    Connecting to quickblok

--- ProChatApp/app/src/main/java/com/example/android/prochatapp/MainActivity.java
+++ ProChatApp/app/src/main/java/com/example/android/prochatapp/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String APP_ID = "77080";
+    private static final String AUTH_KEY = "LMN6DWZvPKbnHYb";
+    private static final String AUTH_SECRET = "UbLVUYqXAP3TsZn";
+    private static final String ACCOUNT_KEY = "yZzhwz3LDsapvX7d_-L7";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
